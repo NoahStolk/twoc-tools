72301d03117f59e2feb5831a843ba03c97b3f3f6
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-07-28 12:20:47 +0200

    Add RNC (m1) implementation

--- src/TwocTools.Core/Internals/Extensions/BinaryReaderExtensions.cs
+++ src/TwocTools.Core/Internals/Extensions/BinaryReaderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using System.Text;
 
 namespace TwocTools.Core.Internals.Extensions;
 
@@ -7,5 +8,14 @@ public static class BinaryReaderExtensions
 	public static Vector3 ReadVector3(this BinaryReader binaryReader)
 	{
 		return new Vector3(binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle());
+	}
+
+	public static string ReadNullTerminatedString(this BinaryReader br)
+	{
+		StringBuilder sb = new();
+		char c;
+		while ((c = br.ReadChar()) != 0)
+			sb.Append(c);
+		return sb.ToString();
 	}
 }
---
+++ src/TwocTools.Core/Internals/Rnc/RncStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace TwocTools.Core.Internals.Rnc;
+
+public enum RncStatus
+{
+	Ok,
+	UnpackedCrcError,
+	FileSizeMismatch,
+	HufDecodeError,
+	PackedCrcError,
+	FileIsNotRnc,
+}
