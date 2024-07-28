fab48719ce588a7ba9864eec800884aa0a907342
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-07-28 20:57:07 +0200

    Add tests for RNC unpack

--- src/TwocTools.Core.Tests/RncTests.cs
+++ src/TwocTools.Core.Tests/RncTests.cs
@@ -1,12 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwocTools.Core.Internals.Rnc;
 
 namespace TwocTools.Core.Tests;
 
 [TestClass]
 public class RncTests
 {
-	[TestMethod]
-	public void CratesNus()
+	[DataTestMethod]
+	[DataRow("PAUSE")]
+	[DataRow("CRATES")]
+	public void ExtractGscToNus(string fileName)
 	{
+		byte[] gscInput = File.ReadAllBytes(Path.Combine("Resources", $"{fileName}.GSC"));
+		byte[] expectedNusInput = File.ReadAllBytes(Path.Combine("Resources", $"{fileName}.NUS"));
+
+		byte[] nusOutput = RncMethod2.Unpack(gscInput);
+
+		Assert.AreEqual(expectedNusInput.Length, nusOutput.Length);
+
+		for (int i = 0; i < expectedNusInput.Length; i++)
+			Assert.AreEqual(expectedNusInput[i], nusOutput[i]);
 	}
 }
